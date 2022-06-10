71ec40e75eb8d9ae7a14809be984c9644bc3c694
ppun1129 <ppun1129@gmail.com>
2022-06-10 14:18:18 +0900

    Initial commit

---
+++ src/main/java/com/pickerbell/api/PickerbellApiApplication.java
@@ -0,0 +1,13 @@
+package com.pickerbell.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickerbellApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickerbellApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pickerbell/api/PickerbellApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pickerbell.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickerbellApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
