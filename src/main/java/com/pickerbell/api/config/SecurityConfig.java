8130f60ec0702039a70f2745be2392074fb9a97f
daeyoon <lightviolet7@gmail.com>
2023-06-19 23:00:45 +0900

    SecurityConfig 클래스의 생성자를 어노테이션을 통해 처리(RequiredArgsConstructor)

--- src/main/java/com/pickerbell/api/config/SecurityConfig.java
+++ src/main/java/com/pickerbell/api/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.pickerbell.api.config;
 
 import com.pickerbell.api.security.filter.JwtFilter;
 import com.pickerbell.api.security.component.JwtOperation;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
